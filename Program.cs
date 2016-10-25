e21663733f173ff82b40d217ff2a78e357b13df4
Nilesh <nickaggarwal>
2016-10-25 11:14:22 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TestCSharp
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+		public void doSomething(int num, int size)
+		{
+			int temp = num ;
+			size = temp ;
+			num = temp + 1;
+		}
+	}
+}
