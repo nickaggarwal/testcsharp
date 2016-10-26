994bc20cb99eef6cd49e00864bdf20865d3004bf
Nilesh <nickaggarwal>
2016-10-26 19:40:17 +0530

    Added GC collect

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace TestCSharp
 		public static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			long mem1 = GC.GetTotalMemory(false);
+			    // Allocate an array and make it unreachable.
+		    int[] values = new int[50000];
+		    values = null;
+			long mem2 = GC.GetTotalMemory(false);
+			    // Collect garbage.
+		    GC.Collect();
+			long mem3 = GC.GetTotalMemory(false);
+		    Console.WriteLine(mem1);
+		    Console.WriteLine(mem2);
+		    Console.WriteLine(mem3);
 		}
 
 		public void doSomething(int num, int size)
